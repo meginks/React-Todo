a0f3893a5bc8044e1b4fab696eb69cd958fe96a9
unknown <38929577+meginks@users.noreply.github.com>
2019-02-13 15:27:03 -0600

    added TodoList class and added state w/ data to app class

--- src/App.js
+++ src/App.js
@@ -4,10 +4,33 @@ class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+  state = {
+    todos: [
+      {task: 'clean bathroom', 
+      id: 1, 
+      completed: false}, 
+      
+      {task: 'take out trash', 
+      id: 2, 
+      completed: false}, 
+
+      {task: 'cook dinner', 
+      id: 3, 
+      completed: false}, 
+
+      {task: 'do laundry', 
+      id: 4, 
+      completed: false}
+    ]
+  }
+
+
   render() {
     return (
       <div>
-        <h2>Welcome to your Todo App!</h2>
+        <h2>To Do List</h2>
+        <TodoList 
+        todos={this.state.todos} />
       </div>
     );
   }
--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,17 @@
-// your components will all go in this `component` directory.
-// feel free to change this component.js into TodoList.js
+import React from 'react';
+
+
+class TodoList extends React.Component {
+    constructor() {
+        super();
+    }
+
+    render() {
+        return (
+            <div>
+                <h1> To Do List</h1>
+            </div>
+        )
+    }
+
+}
